977e0fb2f9e483a80dd04e892ac08552229fb1a8
Jody Lent <jodylent@gmail.com>
2017-03-12 14:13:26 -0500

    default query plus optional query arg

--- runner.py
+++ runner.py
@@ -1,8 +1,26 @@
+import argparse
+
 from boolos.model import Pantheon
 from boolos.util import test_query
 
-p = Pantheon()
-# q = "1 == 1"
-q = "isinstance(a, F_God)"
+if __name__ == "__main__":
 
-test_query(p, q)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", "-q",
+                        action="store",
+                        dest="query",
+                        type=str,
+                        default=None,
+                        help="query")
+    args = parser.parse_args()
+
+    # Generate a pantheon
+    p = Pantheon()
+
+    # run the query
+    if args.query:
+        test_query(p, args.query)
+    else:
+        print("RUNNING DEFAULT QUERY")
+        q = "isinstance(a, F_God)"
+        test_query(p, q)
